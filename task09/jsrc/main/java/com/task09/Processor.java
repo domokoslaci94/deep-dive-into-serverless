ac4f2596331101d0026ee7b0ce8b860157a6ef00
domokoslaci94 <domokoslaci94@gmail.com>
2024-05-13 21:54:35 +0200

    Refactor to use DynamoDbEnhancedClient

--- task09/jsrc/main/java/com/task09/Processor.java
+++ task09/jsrc/main/java/com/task09/Processor.java
@@ -1,7 +1,5 @@
 package com.task09;
 
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -12,44 +10,48 @@ import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
 import com.syndicate.deployment.model.RetentionSetting;
 import com.syndicate.deployment.model.TracingMode;
 import com.task09.dao.WeatherDao;
+import com.task09.dao.entity.WeatherEntity;
 import com.task09.service.WeatherService;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
 
 import java.net.http.HttpClient;
 import java.util.HashMap;
 import java.util.Map;
 
 @LambdaHandler(lambdaName = "processor",
-	roleName = "processor-role",
-	tracingMode = TracingMode.Active,
-	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+        roleName = "processor-role",
+        tracingMode = TracingMode.Active,
+        logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
 @EnvironmentVariables(value = {
-		@EnvironmentVariable(key = "region", value = "${region}"),
-		@EnvironmentVariable(key = "target_table", value = "${target_table}")
+        @EnvironmentVariable(key = "region", value = "${region}"),
+        @EnvironmentVariable(key = "target_table", value = "${target_table}")
 })
 @LambdaUrlConfig
 public class Processor implements RequestHandler<Object, Map<String, Object>> {
 
-	private static final String REGION = System.getenv("region");
-	private static final String TARGET_TABLE = System.getenv("target_table");
+    private static final String REGION = System.getenv("region");
+    private static final String TABLE_NAME = System.getenv("target_table");
 
-	private static final WeatherService WEATHER_SERVICE = new WeatherService(HttpClient.newHttpClient(), new ObjectMapper());
-	private static final WeatherDao WEATHER_DAO = new WeatherDao(configureDynamoDB(), TARGET_TABLE);
+    private static final WeatherService WEATHER_SERVICE = new WeatherService(HttpClient.newHttpClient(), new ObjectMapper());
+    private static final DynamoDbTable<WeatherEntity> WEATHER_TABLE = configureDynamoDBTable();
+    private static final WeatherDao WEATHER_DAO = new WeatherDao(WEATHER_TABLE);
 
-	private static AmazonDynamoDB configureDynamoDB() {
-		return AmazonDynamoDBClientBuilder.standard()
-				.withRegion(REGION)
-				.build();
-	}
+    private static DynamoDbTable<WeatherEntity> configureDynamoDBTable() {
+        return DynamoDbEnhancedClient.create()
+                .table(TABLE_NAME, TableSchema.fromBean(WeatherEntity.class));
+    }
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
-		System.out.println("Hello from lambda");
+    public Map<String, Object> handleRequest(Object request, Context context) {
+        System.out.println("Hello from lambda");
 
-		WEATHER_DAO.save(WEATHER_SERVICE.queryWeather());
+        WEATHER_DAO.save(WEATHER_SERVICE.queryWeather());
 
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("statusCode", 200);
-		resultMap.put("body", "Hello from Lambda");
-		return resultMap;
-	}
+        Map<String, Object> resultMap = new HashMap<>();
+        resultMap.put("statusCode", 200);
+        resultMap.put("body", "Hello from Lambda");
+        return resultMap;
+    }
 }
--- task09/jsrc/main/java/com/task09/api/WeatherSnippetHourly.java
+++ task09/jsrc/main/java/com/task09/api/WeatherSnippetHourly.java
@@ -1,36 +1,38 @@
 package com.task09.api;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
 
-import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+@DynamoDbBean
 public class WeatherSnippetHourly {
-    private double[] temperature_2m;
-    private String[] time;
+    private List<Double> temperature_2m;
+    private List<String> time;
 
     public WeatherSnippetHourly() {
     }
 
-    public WeatherSnippetHourly(double[] temperature_2m, String[] time) {
+    public WeatherSnippetHourly(List<Double> temperature_2m, List<String> time) {
         this.temperature_2m = temperature_2m;
         this.time = time;
     }
 
-    public double[] getTemperature_2m() {
+    public List<Double> getTemperature_2m() {
         return temperature_2m;
     }
 
-    public void setTemperature_2m(double[] temperature_2m) {
+    public void setTemperature_2m(List<Double> temperature_2m) {
         this.temperature_2m = temperature_2m;
     }
 
-    public String[] getTime() {
+    public List<String> getTime() {
         return time;
     }
 
-    public void setTime(String[] time) {
+    public void setTime(List<String> time) {
         this.time = time;
     }
 
@@ -39,19 +41,19 @@ public class WeatherSnippetHourly {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         WeatherSnippetHourly that = (WeatherSnippetHourly) o;
-        return Objects.deepEquals(temperature_2m, that.temperature_2m) && Objects.deepEquals(time, that.time);
+        return Objects.equals(temperature_2m, that.temperature_2m) && Objects.equals(time, that.time);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(Arrays.hashCode(temperature_2m), Arrays.hashCode(time));
+        return Objects.hash(temperature_2m, time);
     }
 
     @Override
     public String toString() {
         return "WeatherSnippetHourly{" +
-                "temperature_2m=" + Arrays.toString(temperature_2m) +
-                ", time=" + Arrays.toString(time) +
+                "temperature_2m=" + temperature_2m +
+                ", time=" + time +
                 '}';
     }
 }
